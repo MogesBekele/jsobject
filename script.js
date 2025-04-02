ec5da312401148bff38874bd220ec2e63b1aa4f1
MogesBekele <mogesbekele32@gmail.com>
2025-04-02 16:11:04 +0300

    add the scirpt

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let object = {
+  name: 'moges',
+  age: 28,
+  emial:'mogesbekele32@gmail.com',
+  address: {
+    city: 'Addis Ababa',
+    country: 'Ethiopia'
+  },
+  hobbies: ['reading', 'traveling', 'coding'],
+}
