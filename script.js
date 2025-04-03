7195c3f5ea6a431a8a32bd392e7778bca63ebeb8
MogesBekele <mogesbekele32@gmail.com>
2025-04-03 16:48:04 +0300

    commit on the dom manipulation

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-// let object = {
-//   name: 'moges',
-//   age: 28,
-//   emial:'mogesbekele32@gmail.com',
-//   address: {
-//     city: 'Addis Ababa',
-//     country: 'Ethiopia'
-//   },
-//   hobbies: ['reading', 'traveling', 'coding'],
-// }
-// const fruit = {Bananas:300, Oranges:200, Apples:500};
+let object = {
+  name: 'moges',
+  age: 28,
+  emial:'mogesbekele32@gmail.com',
+  address: {
+    city: 'Addis Ababa',
+    country: 'Ethiopia'
+  },
+  hobbies: ['reading', 'traveling', 'coding'],
+}
+const fruit = {Bananas:300, Oranges:200, Apples:500};
 
-// const myMap = new Map(Object.entries(fruit));
+const myMap = new Map(Object.entries(fruit));
 
-// const fruits = [
-//   ["apples", 300],
-//   ["pears", 900],
-//   ["bananas", 500]
-// ];
-// console.log(myMap)// return the map object
-// console.log(Object.keys(object))// return the keys values from the object array
-// console.log(Object.entries(object)) //return the array of the objects
-// console.log(Object.values(object)) // return the values of the object
-// console.log(myObj) // return the object from the array
+const fruits = [
+  ["apples", 300],
+  ["pears", 900],
+  ["bananas", 500]
+];
+console.log(myMap)// return the map object
+console.log(Object.keys(object))// return the keys values from the object array
+console.log(Object.entries(object)) //return the array of the objects
+console.log(Object.values(object)) // return the values of the object
+console.log(myObj) // return the object from the array
 
-// // Create an Array
-// const vegetable = [
-//   {name:"apples", quantity:300},
-//   {name:"bananas", quantity:500},
-//   {name:"oranges", quantity:200},
-//   {name:"kiwi", quantity:150}
-// ];
+// Create an Array
+const vegetable = [
+  {name:"apples", quantity:300},
+  {name:"bananas", quantity:500},
+  {name:"oranges", quantity:200},
+  {name:"kiwi", quantity:150}
+];
 
-// // Callback function to Group Elements
-// function myCallback({ quantity }) {
-//   return quantity > 200 ? "ok" : "low";
-// }
+// Callback function to Group Elements
+function myCallback({ quantity }) {
+  return quantity > 200 ? "ok" : "low";
+}
 
-// // Group by Quantity
-// const result = Object.groupBy(vegetable, myCallback);
+// Group by Quantity
+const result = Object.groupBy(vegetable, myCallback);
 
-// console.log(result);// the result
+console.log(result);// the result
 
-// const myObj = Object.fromEntries(fruits);
+const myObj = Object.fromEntries(fruits);
 
 const button = document.getElementById("button");
 
