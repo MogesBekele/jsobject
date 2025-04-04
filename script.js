c56a36610848cc1297be7ae58d5036bbb0bb8921
MogesBekele <mogesbekele32@gmail.com>
2025-04-04 05:50:22 +0300

    commit on the dom manipulation

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const result = Object.groupBy(vegetable, myCallback);
 console.log(result);// the result
 
 const myObj = Object.fromEntries(fruits);
-console.log(myObj) // return the object from the array
+console.log(myObj) 
 
 
 const button = document.getElementById("button");
