2e99fc1de3caa7cbf6913346a3ef4adee92ed593
MogesBekele <mogesbekele32@gmail.com>
2025-04-04 17:16:56 +0300

    commit on the array method

--- script.js
+++ script.js
@@ -76,6 +76,9 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const newArray = array.filter((item)=> item>4);
 console.log(newArray)// return the filtered array
 
+const newArray1 = array.slice(0, 5);
+console.log(newArray1)// return the sliced array
+
 
 
 
