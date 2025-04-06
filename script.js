a2790b1dfc5414b5445e1d5461e5a525dab1c7d2
MogesBekele <mogesbekele32@gmail.com>
2025-04-06 06:18:35 +0300

    commit on the data fetch mthod

--- script.js
+++ script.js
@@ -235,3 +235,11 @@ const fetchData = async ()=>{
   }
 }
 
+
+
+//function method example
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
