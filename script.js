08c32396d8405d188a83d075446ac8c7714f6946
MogesBekele <mogesbekele32@gmail.com>
2025-04-06 07:06:43 +0300

    commit on generator function

--- script.js
+++ script.js
@@ -340,9 +340,6 @@ console.log(generator.next().value); // Output: World
 
 
 // setTimeout example
-setTimeout(() => {
-  console.log("This message is displayed after 2 seconds.");
-}, 2000); // 2000 milliseconds = 2 seconds
 
 // setInterval example
 setInterval(() => {
