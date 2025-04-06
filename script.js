e1352b864a65797fbec0a5e36b0df790125ccd11
MogesBekele <mogesbekele32@gmail.com>
2025-04-06 06:19:17 +0300

    commit on the data fetch mthod

--- script.js
+++ script.js
@@ -243,3 +243,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+console.log(add(2, 3)); // 5
+console.log(subtract(5, 2)); // 3
+console.log(multiply(2, 3)); // 6
+console.log(divide(6, 3)); // 2
