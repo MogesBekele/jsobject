aef4993c53d0fde9c364e8291cb3ec15189afb4f
MogesBekele <mogesbekele32@gmail.com>
2025-04-06 06:22:21 +0300

    commit on the data fetch mthod

--- script.js
+++ script.js
@@ -263,4 +263,7 @@ const square = (x) => x * x;
 console.log(square(5)); // 25
 
 // higher order function example
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled); // [2, 4, 6, 8, 10]
 
