8f1245d0b8d4e4f74545e60cdfeb3dc34aafa155
MogesBekele <mogesbekele32@gmail.com>
2025-04-20 08:13:31 +0300

    commit on the closure function

--- script.js
+++ script.js
@@ -381,7 +381,7 @@ dog.bark();
 function closure() {
   let count = 0; // A variable defined in the outer function
   return function() {
-    count++;
+    count++;// Increment the count variable
     console.log(count); 
   };
 };
